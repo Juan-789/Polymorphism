085b49aa76b6db0581dc1642764e731e28088732
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-05-04 22:18:17 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -4,24 +4,63 @@ public class Account {
     double balance;
 
     public static void main(String[] args) {
-        System.out.println("Hi!");
-        
+        System.out.print("Input your account number: ");    //asks for acc. Number
+        Scanner accNumObj = new Scanner(System.in);
+        String accNum = accNumObj.nextLine();
+        Account object = new Account();
+        object.setAccountNumber(accNum);
+        accNumObj.close();
+
+        System.out.print("Input your account balance: ");   //asks for acc. balance
+        Scanner accBalObj = new Scanner(System.in);
+        double accBal = accBalObj.nextDouble();
+        object.setBalance(accBal);
+        accBalObj.close();
+
+        System.out.print("Input 'd' for deposit, and 'w' if you want to perform a withdrawal ");
     }
-    Account(String accountNumber, double balance) {
+
+    public void setAccountNumber(String accountNumber) {    //sets acc. number
         this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {            //sets balance
         this.balance = balance;
     }
+
     double deposit() {
         Scanner depositObj = new Scanner(System.in);
-        System.out.print("How much would you like to deposit? ");
+        System.out.print("How much would you like to deposit? ");   //asks for deposit
         double depositedAmount = depositObj.nextDouble();
-        return balance+depositedAmount;
-
+        depositObj.close();
+        return (balance+depositedAmount);
     }
     double withdraw() {
         Scanner withdrawObj = new Scanner(System.in);
-        System.out.print("How much would you like to deposit? ");
+        System.out.print("How much would you like to deposit? ");   //asks for withdrawal
         double withdrawAmount = withdrawObj.nextDouble();
-        return balance-withdrawAmount;
+        withdrawObj.close();
+        return (balance-withdrawAmount);
     }
 }
+
+class CheckingAccount extends Account{
+    double overdraftLimit() {       //overdraft limit
+    return(balance+50);
+    }
+
+    double overdraft(){
+        CheckingAccount overdObj = new CheckingAccount();   //when an overdraft happens
+        double overWithdrawal = overdObj.withdraw();
+        return (balance-overWithdrawal);
+    }
+}
+
+class SavingsAccount extends Account{
+    double interestRate = 0.03;         //savings account interest
+    double calculateInterest() {
+        return (balance*interestRate);
+    }
+}
+//asks to choose an account to register
+//then store the data and polymorph the methods
