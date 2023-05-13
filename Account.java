502b585d373e66571348884b510ba266e6b12527
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-05-13 00:24:50 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,39 +1,33 @@
 public class Account {
     String accountNumber;
     double balance;
-    public static void main(String[] args) {
-//        make constructor of data
-        Account person1 = new Account("CJ8923", 0);
-        person1.s
-    }
 
     Account (String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
-
     double deposit() {
-
         double depositedAmount = 300;
-
         double newBalance = balance+depositedAmount;
         return (newBalance);
     }
+
     double withdraw() {
         return (balance-50);
     }
 
     void transctioned_amount(){
-        System.out.print("Account with number: "+accountNumber+" has the following balance, after the "+" transaction occurred.");   //asks for withdrawal
+        System.out.println("Account # has done the following transaction");
     }
+
 }
 
 class CheckingAccount extends Account{
     double overdraftLimit;       //overdraft limit
-    CheckingAccount(double overdraftLimit){
-        super("CJ8923", 0);
-        overdraftLimit = 100;
+    CheckingAccount(String accountNumber, double balance, double overdraftLimit){
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
     }
     boolean overdraft(){
 //        CheckingAccount overdObj = new CheckingAccount();   //when an overdraft happens
@@ -47,14 +41,17 @@ class CheckingAccount extends Account{
     //make if statement of overdraft, and new balance
     @Override
     void transctioned_amount() {
-        System.out.println("Account with number: CJ8923 has");
+        if (overdraft() == true){
+        System.out.println("Account with number: CJ8923 has overdrafted");
+        }
     }
 }
 
 class SavingsAccount extends Account{
     double interestRate;         //savings account interest
-    SavingsAccount(double interestRate){
-        super("S45667", 700);
+
+    SavingsAccount(String accountNumber, double balance,double interestRate){
+        super(accountNumber, balance);
         this.interestRate = interestRate;
     }
     double calculateInterest() {
@@ -62,7 +59,18 @@ class SavingsAccount extends Account{
     }
     @Override
     void transctioned_amount(){
-        System.out.println();
+//        SavingsAccount SavObj = new SavingsAccount(0.03);
+        System.out.println("Account number: "+accountNumber+" will accrue a total of $"+calculateInterest()+" a year from now.");
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Account person1 = new Account("CJ8923", 1000);
+        Account person2 = new CheckingAccount("CJ365",0,50);
+        Account person3 = new SavingsAccount("SA765", 0, 0);
+        person2.withdraw();
+        
+        person2.transctioned_amount();
     }
 }
 //asks to choose an account to register
